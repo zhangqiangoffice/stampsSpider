7e314b5214f20dc735cb4a85d6bd2c6ff07ae10f
Quentin Zhang <quentinzhang@zillionsource.com>
2019-02-28 13:03:02 +0800

    feat: 将92-19年信息抓取并保存成 json 文件

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const request = require('request')
 const cheerio = require('cheerio')
+const fs = require('fs')
 
 
-const download = url => {
+const download = (url, handleBody) => {
     console.log('下载：', url)
     request(url, function (error, response, body) {
         if (error) {
             console.log('error:', error); // Print the error if one occurred
         } else {
             if (response && response.statusCode === 200) {
-                $ = cheerio.load(body);
-                const target = $('.stamp_shop_list').text();
-                console.log('target: ', target);  
+                if(handleBody) {
+                    handleBody(body)
+                } else {
+                    console.log('unhandle body:', body)
+                }
             } else {
                 console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
             }
@@ -19,6 +22,58 @@ const download = url => {
     });
 }
 
+const writeFile = (path, data) => {
+    fs.writeFile(path, data, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("保存：" + path);
+        }
+    });
+}
+
+const handleBody = body => {
+    $ = cheerio.load(body);
+    const list = [];
+    
+    //生成 邮票套 对象数组
+    $('.stamp_shop_list').children().each((i, el) => {
+        const stampSet = {}
+        const $el = $(el);
+
+        stampSet.detailUrl = $el.find('.stamp_imgs').attr('href');
+        stampSet.imgSrc = $el.find('.stamp_imgs img').attr('src');
+        stampSet.title = $el.find('.stamp_shop_name').text();
+        stampSet.recordNum = $el.find('.stamp_num').text().slice(4);
+        
+        const averagePriceStr = $el.find('.stamp_average_price_shop').text();
+        stampSet.averagePrice = averagePriceStr.substring(7, averagePriceStr.length - 2);
+        
+        list.push(stampSet)
+    })
+
+    //个别瑕疵处理
+    list.forEach(obj => {
+        
+        //标题中左括号使用英文了
+        if (obj.title.includes('(')) {
+            console.log(`修复：英文左括号 - ${obj.title}`);
+            obj.title = obj.title.replace(/\(/g, '（')
+        }
+        
+        //标题中右括号使用英文了
+        if (obj.title.includes(')')) {
+            console.log(`修复：英文右括号 - ${obj.title}`);
+            obj.title = obj.title.replace(/\)/g, '）')
+        }
+
+    })
+    
+    //保存成本地 json 文件
+    var outputFilename = './data/sets.json';
+    writeFile(outputFilename, JSON.stringify(list, null, 4));
+}
+
 const url = 'http://www.chinau.cc/search/searchList.html?orderByIncrease=&orderByReleaseDate=ASC&pageSize=900&currentPage=1&stampCategoryId=512'
 
-download(url);
+download(url, handleBody);
